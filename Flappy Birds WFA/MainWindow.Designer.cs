c7b7491f9c4b4edf6af01939f5cc94a06748e6d6
Tobiazsh <51285655+tobiazsh@users.noreply.github.com>
2025-11-14 19:13:33 +0100

    Add Icon and make game window unresizable

--- Flappy Birds WFA/MainWindow.Designer.cs
+++ Flappy Birds WFA/MainWindow.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             SuspendLayout();
             // 
             // MainWindow
@@ -35,6 +36,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MainWindow";
             Text = "Form1";
             Load += MainWindow_Load;
